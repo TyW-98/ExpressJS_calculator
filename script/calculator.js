5af03c776e651436a0ccb540bade2989baf729d5
wey1998 <yitwey1998@hotmail.com>
2023-04-11 00:17:33 +0100

    Add separate route for each calculator

--- script/calculator.js
+++ script/calculator.js
@@ -6,23 +6,24 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Basic addition calculator
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../index.html"));
-// });
-
-// app.post("/", (req, res) => {
-//     var num1 = Number(req.body.num1)
-//     var num2 = Number(req.body.num2)
-//     var sum = num1 + num2;
-//     res.send("The result is " + sum);
-//     console.log(sum);
-// })
-
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname,"../bmiCalculator.html"));
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
 
 app.post("/", (req, res) => {
+    var num1 = Number(req.body.num1)
+    var num2 = Number(req.body.num2)
+    var sum = num1 + num2;
+    res.send("The result is " + sum);
+    console.log(sum);
+})
+
+// Basic BMI Calculator
+app.get("/bmicalculator", (req, res) => {
+  res.sendFile(path.join(__dirname,"../bmiCalculator.html"));
+});
+
+app.post("/bmicalculator", (req, res) => {
   var weight = Number(req.body.weight);
   var height = Number(req.body.height);
   var BMI = weight/(height**2);

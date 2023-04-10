44a21a5e29e7eb6c49dd608ec8af20daf947fb4b
wey1998 <yitwey1998@hotmail.com>
2023-04-11 00:19:13 +0100

    Use parseFloat instead of Number to convert weight and height number from string to float

--- script/calculator.js
+++ script/calculator.js
@@ -24,8 +24,8 @@ app.get("/bmicalculator", (req, res) => {
 });
 
 app.post("/bmicalculator", (req, res) => {
-  var weight = Number(req.body.weight);
-  var height = Number(req.body.height);
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
   var BMI = weight/(height**2);
   res.send("Your BMI is "+ BMI);
 });

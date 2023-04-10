41a3766f665d3a8861c6d039de22a71c8c92ddc2
wey1998 <yitwey1998@hotmail.com>
2023-04-11 00:14:43 +0100

    Add basic BMI calculator

--- script/calculator.js
+++ script/calculator.js
@@ -5,18 +5,32 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Basic addition calculator
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../index.html"));
+// });
+
+// app.post("/", (req, res) => {
+//     var num1 = Number(req.body.num1)
+//     var num2 = Number(req.body.num2)
+//     var sum = num1 + num2;
+//     res.send("The result is " + sum);
+//     console.log(sum);
+// })
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname,"../bmiCalculator.html"));
 });
 
 app.post("/", (req, res) => {
-    var num1 = Number(req.body.num1)
-    var num2 = Number(req.body.num2)
-    var sum = num1 + num2;
-    res.send("The result is " + sum);
-    console.log(sum);
-})
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+  var BMI = weight/(height**2);
+  res.send("Your BMI is "+ BMI);
+});
+
 
 app.listen(3000, function (req, res) {
   console.log("Server running on port 3000");
 });
+
